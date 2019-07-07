6c4ae3cf97c63d28fe42bde9a98d1774618107f3
Aiswarya Das <dasaiswarya1993@gmail.com>
2019-07-07 15:10:59 +0800

    adds main method

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,7 +1,7 @@
 import math
 
-
-def is_leap(y):
+# to check whether the year is leap year or not.
+def is_leap_year(y):
     leap = False
     if 1900 <= y <= 1 * math.pow(10, 5):
         if y % 4 == 0 or y % 400 == 0:
@@ -14,7 +14,7 @@ def is_leap(y):
 
 
 year = int(input())
-print(is_leap(year))
+print(is_leap_year(year))
 
 
 
@@ -37,7 +37,9 @@ def is_leap(year):
     return leap
 
 
-year = int(input())
-print(is_leap(year))
+if __name__ == "__main__":
+    year = int(input())
+    print(is_leap(year))
 
-
+    # year = int(input())
+    # print(is_leap_year(year))
