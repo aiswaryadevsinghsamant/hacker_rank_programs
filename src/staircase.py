50866aa57361083c18af170d2f7d264cf35ccd67
Aiswarya Das <dasaiswarya1993@gmail.com>
2019-07-07 14:47:00 +0800

    adds a program to create staircase using #

--- src/staircase.py
+++ src/staircase.py
@@ -1,12 +1,4 @@
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the staircase function below.
-
-
+# builds a staircase.
 def staircase(n):
     if n in range(1, 101):
         for num in range(1, n+1):
